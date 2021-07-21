d4f70bcd81d0b76d4e01d77568392b19d95d0363
hhl <rizanahter@VICIVICI.local>
2021-07-21 17:51:27 +0300

    Initial commit

---
+++ lib/degiskenolusturma.dart
@@ -0,0 +1,22 @@
+void main(){
+  var ogrenciAdi="Aynur";
+  var ogrenciYasi=20;
+  var ogrenciBoyu=1.63;
+  var ogrenciBasHarfi="A";
+  print(ogrenciAdi);
+  print(ogrenciYasi);
+  print(ogrenciBoyu);
+  print(ogrenciBasHarfi);
+
+  int urun_id=3416;
+  String urun_adi="Kol saati";
+  int urun_adet=100;
+  double urun_fiyat=149.99;
+  String urun_tedarikci="rolex";
+  print("ürün id: $urun_id");
+  print("ürün adı: $urun_adi");
+  print("ürün adet: $urun_adet");
+  print("ürün fiyat: $urun_fiyat");
+  print("ürün tedarikçi :$urun_tedarikci");
+
+}
